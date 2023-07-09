6b59859042ff97f268ae6484f13b3d6bcabc8df0
Hashimjasem <j_hashim1305@outlook.com>
2023-07-09 12:44:23 +0800

    Header

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <div>
+            <h1 className=" 
+            bg-gradient-to-b from-[#640e71] from-30% to-transparent to-100%
+            h-[10rem]
+             text-white
+            font-bold 
+            p-[1rem]
+            font-mono 
+            text-[3rem]">HASHIM JASEM</h1>
+        </div>
+    );
+}
+
+export default Header;
