a9a4a0742638c68f448584b508cff6119aa20599
Hashimjasem <j_hashim1305@outlook.com>
2023-07-24 16:18:08 +0800

    gradient fliped on 2nd row

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,7 @@ function About() {
                 <p className="w-[65%] self-center m-auto">I am a 21 year old aspiring software developer that is eager to get in the industry and learn. I have been coding for over a year now and i have fallen in-love with the process of becoming a better programmer. </p>
             </div>
 
-            <div className="my-[1rem] bg-gradient-to-r from-[#640e71] to-[#bb24d2] w-full h-[10rem] rounded-full inline-flex">
+            <div className="my-[1rem] bg-gradient-to-r from-[#bb24d2] to-[#640e71] w-full h-[10rem] rounded-full inline-flex">
                 <p className="w-[65%] self-center m-auto">I am a 21 year old aspiring software developer that is eager to get in the industry and learn. I have been coding for over a year now and i have fallen in-love with the process of becoming a better programmer. </p>
                 <div className="bg-white w-[10rem] h-[10rem] rounded-full"></div>
             </div>
